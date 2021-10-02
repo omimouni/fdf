0b6e43ade12186673ebe6bc1c9a7c0135d9ca0ac
Oumar Mimouni <omimouni@student.1337.ma>
2021-10-02 15:35:20 +0100

    correcting the map direction

--- src/engine/render.c
+++ src/engine/render.c
@@ -6,54 +6,46 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/02 14:13:31 by omimouni          #+#    #+#             */
-/*   Updated: 2021/10/02 15:03:23 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/10/02 15:35:06 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
 void
-	f_render_points(t_point *p, int nz, t_fdf *fdf)
-{
-	t_point	*cp;
-	t_point	*np;
-	int		buff;
-
-	cp = point_create(0, 0, 0);
-	np = point_create(0, 0, 0);
-	f_iso_project(*p, &(cp->x), &(cp->y));
-	buff = p->x;
-	p->x += fdf->zoom_scale;
-	f_iso_project(*p, &(np->x), &(np->y));
-	f_draw_line(cp->x + fdf->offset_x, cp->y + fdf->offset_y,
-	 np->x + fdf->offset_x, np->y + fdf->offset_y, 0, fdf);
-	p->x = buff;
-	p->y += fdf->zoom_scale;
-	f_iso_project(*p, &(np->x), &(np->y));
-	f_draw_line(cp->x + fdf->offset_x, cp->y + fdf->offset_y
-		, np->x + fdf->offset_x, np->y + fdf->offset_y, 0, fdf);
-}
-
-void
 	f_render(t_fdf *fdf)
 {
 	int	x;
 	int	y;
+	int	rx;
+	int	ry;
 	t_point	*p;
 
-	x = 0;
-	p = point_create(0, 0, 0);
-	while (x < fdf->map_h)
+	int	fd;
+	fd = open("test_maps/test.fdf", O_RDONLY);
+	char	*line;
+	char	**tab;
+	y = 0;
+	while (y <= fdf->map_h)
 	{
-		y = 0;
-		while (y < fdf->map_w)
+		line = ft_gnl(fd);
+		tab = ft_split(line, ' ');
+		x = 0;
+		while (x <= fdf->map_w)
 		{
-			p->x = x * fdf->zoom_scale;
-			p->y = y * fdf->zoom_scale;
-			p->z = 0;
-			f_render_points(p, 0, fdf);
-			y++;
+			printf("%d %d\n", x, y);
+			rx = x * fdf->zoom_scale;
+			ry = y * fdf->zoom_scale;
+			if (x != fdf->map_w)
+				f_draw_line(rx, ry, ((x + 1) * fdf->zoom_scale), ry, 0, fdf);
+			if (y != fdf->map_h)
+				f_draw_line(rx, ry, rx, (y +1) * fdf->zoom_scale, 0, fdf);
+			// f_put_pixel(x * fdf->zoom_scale, y * fdf->zoom_scale, 0xFFFFFF, fdf);
+			// f_put_pixel((x + 1) * fdf->zoom_scale, y * fdf->zoom_scale, 0xFF0000, fdf);
+			x+=1;
 		}
-		x++;
+		ft_split_free(tab, fdf->map_w);
+		free(line);
+		y++;
 	}	
 }
